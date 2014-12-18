b935ac7d7ed7c40ae4a454692a75b0258514e7f1
Ryan Clark <rclark.sf@gmail.com>
2014-12-18 12:36:46 -0800

    some more tweaks

--- Rakefile
+++ Rakefile
@@ -1,4 +1,9 @@
 namespace :db do
+  desc "Provide db:setup so Ninefold can run the command"
+  task :setup do
+    puts " :) "
+  end
+  
   desc "Provide db:migrate so Ninefold can run the command"
   task :migrate do
     puts " :) "
--- app.rb
+++ app.rb
@@ -1,11 +1,9 @@
 require 'sinatra'
 
-class Main < Sinatra::Base
-  get '/' do
-    if settings.development?
-      "development!"
-    else
-      "Sinatra on Ninfold!"
-    end
+get '/' do
+  if settings.development?
+    "development!"
+  else
+    "Sinatra on Ninfold!"
   end
 end
